7aad0bd0da3e405f26dbff715c0273d83a9507b9
siyalnaeem97 <naeem97ali@gmail.com>
2020-09-29 10:06:09 +0500

    Project 4a done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
-import useWebAnimations from "@wellyshen/use-web-animations";
 import React from "react";
-import "./App.css";
+import { RedQueen } from "./component/redQueen/redQueen.component";
 
 function App() {
-  const { ref, playState } = useWebAnimations({
-    keyframes: {
-      transform: "translateX(500px)", // Move by 500px
-      background: ["red", "blue", "green"], // Go through three colors
-    },
-    timing: {
-      delay: 500, // Start with a 500ms delay
-      duration: 1000, // Run for 1000ms
-      iterations: 2, // Repeat once
-      direction: "alternate", // Run the animation forwards and then backwards
-      easing: "ease-in-out", // Use a fancy timing function
-    },
-    onReady: ({ playState, animate, animation }) => {
-      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-    },
-    onUpdate: ({ playState, animate, animation }) => {
-      // Triggered when the animation enters the running state or changes state
-    },
-    onFinish: ({ playState, animate, animation }) => {
-      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-    },
-    // More useful options...
-  });
-
-  return (
-    <div className="container">
-      <p>🍿 Animation is {playState}</p>
-      <div className="target" ref={ref} />
-    </div>
-  );
+  return <RedQueen />;
 }
 
 export default App;
